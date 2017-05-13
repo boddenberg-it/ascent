3463f52538009019e093a99bbd9ba000b5dd12fe
André Boddenberg <andre@boddenberg.it>
2017-05-13 23:52:07 +0200

    shorten colors and remove bold/boldoff

--- ascent.sh
+++ ascent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 help() {
-	echo -e "${YELLOW}#######################################################"
+	echo -e "${Y}#######################################################"
 	echo -e "#" ${b}A${bo}ndroid Semiautomated CEllular Network Testing
 	echo -e "#"
 	echo -e "#"
@@ -11,7 +11,7 @@ help() {
 
 sanity() {
 	echo
-	echo -e "${YELLOW}[SANITY_CHECK] are both devices connected?${NC}"
+	echo -e "${Y}[SANITY_CHECK] are both devices connected?${NC}"
 
 	err_codes=0
 	adb devices | grep "$(serial_of "$d0")"
@@ -21,11 +21,11 @@ sanity() {
 
 	if [ "$err_codes" -gt 0 ]; then
 		echo
-		echo -e "${RED}[ERROR] not all devices are not connected!!!${NC}"
+		echo -e "${R}[ERROR] not all devices are not connected!!!${NC}"
 		echo
 		return 1
 	else
-		echo -e "${GREEN}[INFO] sanity check successful${NC}"
+		echo -e "${G}[INFO] sanity check successful${NC}"
 		echo
 	fi
 }
@@ -67,7 +67,7 @@ data() {
 # actual tests
 send_sms() {
 	if [ $# -ne 2 ]; then
-		echo -e "${RED}[ERROR] You need to pass 2 arguments to send_sms()"
+		echo -e "${R}[ERROR] You need to pass 2 arguments to send_sms()"
 		echo
 		echo "	send_sms \$sender_serial \$receiver_number ${NC}"
 		echo
@@ -75,7 +75,7 @@ send_sms() {
 	fi
 
 	go_to_homescreen
-	echo -e "$YELLOW[TEST-SMS] ${GREEN}$1${YELLOW} sends SMS to ${GREEN}$2${YELLOW} ${NC}"
+	echo -e "$Y[TEST-SMS] ${G}$1${Y} sends SMS to ${G}$2${Y} ${NC}"
 
 	adb -s "$(serial_of "$1")" shell am start -a android.intent.action.SENDTO \
 		-d sms:"$(number_of "$2")" --es sms_body "test_intent" --ez exit_on_sent true
@@ -93,24 +93,24 @@ send_sms() {
 
 do_call() {
 	go_to_homescreen
-	echo -e "${YELLOW}[TEST-CALL] ${GREEN}$1${YELLOW} calls ${GREEN}$2${YELLOW} ${NC}"
+	echo -e "${Y}[TEST-CALL] ${G}$1${Y} calls ${G}$2${Y} ${NC}"
 
 	adb -s "$(serial_of "$1")" shell am start -a android.intent.action.CALL \
                 -d tel:"$(number_of "$2")"
 
-	echo -e "${YELLOW}	${BLUE}[INPUT]${YELLOW} does it ring? (no|ENTER)${NC}"
+	echo -e "${Y}	${B}[INPUT]${Y} does it ring? (no|ENTER)${NC}"
 	read does_it_ring
 
 	if [[ "$does_it_ring" == *"n"* ]]; then
-		echo -e "${RED} 	[ERROR] call could not be established! ${NC}"
+		echo -e "${R} 	[ERROR] call could not be established! ${NC}"
 	else
-		echo -e "${YELLOW}	[INFO] ${GREEN}$2${YELLOW} accepts call${NC}"
+		echo -e "${Y}	[INFO] ${G}$2${Y} accepts call${NC}"
 		adb -s "$(serial_of "$2")" shell input keyevent "$KEYCODE_CALL"
 
-		echo -e "${YELLOW}	${BLUE}[INPUT]${YELLOW} enough of talking? ${NC}"
+		echo -e "${Y}	${B}[INPUT]${Y} enough of talking? ${NC}"
 		read
 
-		echo -e "${YELLOW}	[INFO] ${GREEN}$1${YELLOW} ends call ${NC}"
+		echo -e "${Y}	[INFO] ${G}$1${Y} ends call ${NC}"
 		adb -s "$(serial_of "$1")" shell  input keyevent "$KEYCODE_ENDCALL"
 	fi
 
@@ -119,7 +119,7 @@ do_call() {
 }
 
 ping() {
-	echo -e "${YELLOW}[TEST-DATA] ${GREEN}$1${YELLOW} tries to ping ${GREEN}$2${YELLOW} ${NC}"
+	echo -e "${Y}[TEST-DATA] ${G}$1${Y} tries to ping ${G}$2${Y} ${NC}"
 	adb -s "$(serial_of "$1")" shell ping -c 3 "$2"
 	# TODO: check whether "operation permitted" of s3, can be caught and then 'adb shell su -c ping'
 	echo
@@ -149,13 +149,10 @@ KEYCODE_ENTER=66
 
 # colours
 NC='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;35m'
-YELLOW='\033[1;33m'
-# style
-b=`tput bold`  # bold
-bo=`tput sgr0` # bold off
+R='\033[0;31m'
+G='\033[0;32m'
+B='\033[0;35m'
+Y='\033[1;33m'
 
 # only one char is allowed in fact of using cut.
 # please allign your config file accordingly.
