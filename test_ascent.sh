9ac5ff750063722b765fa5fe8245b4eb0765dabe
André Boddenberg <andre@boddenberg.it>
2017-05-21 00:24:03 +0200

    Introduce automated verification of test_case within script.
    Now, one can use the test_case() function, where the NAME of the
    testcase, the config used for this testcase and the expected error
    code can be passed e.g.:
    
    test_case "MY_FANCY_TEST" "MY_FANCY_CONFIG_FILE" 1

--- test_ascent.sh
+++ test_ascent.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-# Simple script to verify config parsing. The color of [TESTCASE] lines
-# indicate expected test result.
+# Simple script to allow some testing (mainly verifying config parsing).
 
 # colors
 NC="\033[0m"
@@ -9,19 +8,45 @@ R="\033[0;31m"
 G="\033[0;32m"
 B="\033[0;35m"
 
-echo
-echo -e "${R}[TESTCASE] EMPTY CONFIG ${NC}"
-./ascent -c "configs_for_tests/empty_config" "dryrun"
+# counting all errors occuring in test run
+errors=0
 
-echo -e "${R}[TESTCASE] INVALID CONFIG ${NC}"
-./ascent -c "configs_for_tests/invalid_config" "dryrun"
+test_case() {
+  echo
+  echo -e "${B}[TESTCASE] $1 ${NC}"
+  ./ascent -c "configs_for_tests/$2" "testing"
 
-echo -e "${B}[INFO] Please connect ONE device now...${NC}"
-read
-echo -e "${R}[TESTCASE] ONLY ONE DEVICE IS CONNECTED ${NC}"
-./ascent -c "configs_for_tests/valid_config" "dryrun"
+  if [ $? -ne $3 ]; then
+    errors=$((errors+1))
+    echo -e "${R}[TESTCASE] $1 FAILED!!!${NC}"
+    echo
+  else
+    echo -e "${G}[TESTCASE] $1 SUCCEEDED${NC}"
+    echo
+  fi
+}
 
-echo -e "${B}[INFO] Please connect BOTH devices now...${NC}"
-read
-echo -e "${G}[TESTCASE] VALID CONFIG ${NC}"
-./ascent -c "configs_for_tests/valid_config" "dryrun"
+summarise_suites() {
+  echo
+  if [ $errors -eq 0 ]; then
+    echo -e "${G}[RESULT] ALL TESTCASES DID PASS SUCCESSFULLY${NC}"
+    echo
+  else
+    echo -e "${R}[RESULT] $errors TESTCASE(S) FAILED !!!${NC}"
+    echo
+    # return amount of errors in case one wants to evaluate it programmatically
+    exit "$errors"
+  fi
+}
+
+test_case "EMPTY CONFIG" "empty_config" 1
+
+test_case "INVALID CONFIG" "invalid_config" 1
+
+echo -e "${B}[INFO] Please connect ONE device now...${NC}"; read
+test_case "ONLY ONE DEVICE IS CONNECTED" "valid_config" 1
+
+echo -e "${B}[INFO] Please connect BOTH devices now...${NC}"; read
+test_case "ONLY ONE DEVICE IS CONNECTED" "valid_config" 0
+
+summarise_suites
