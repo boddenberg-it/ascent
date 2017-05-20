b7c62011879da7b6bdee3d0903aecd1299f88cba
André Boddenberg <andre@boddenberg.it>
2017-05-20 23:46:36 +0200

    Change order of config tests and adding documentation.

--- test_ascent.sh
+++ test_ascent.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 #
-#
-#
-echo "[INFO] Please connect your devices now..."
+# Simple script to verify config parsing. The color of [TESTCASE] lines
+# indicate expected test result.
+
+# colors
+NC="\033[0m"
+R="\033[0;31m"
+G="\033[0;32m"
+B="\033[0;35m"
+
+echo
+echo -e "${R}[TESTCASE] EMPTY CONFIG ${NC}"
+./ascent -c "configs_for_tests/empty_config" "dryrun"
+
+echo -e "${R}[TESTCASE] INVALID CONFIG ${NC}"
+./ascent -c "configs_for_tests/invalid_config" "dryrun"
+
+echo -e "${B}[INFO] Please connect ONE device now...${NC}"
 read
-echo "[TESTCASE] emtpty config"
-./ascent -c "test/empty_config" "dryrun"
-echo "[TESTCASE] invalid config"
-./ascent -c "test/invalid_config" "dryrun"
-echo "[TESTCASE] valid config"
-./ascent -c "test/valid_config" "dryrun"
-echo "[INFO] Please disconnect one device now"
+echo -e "${R}[TESTCASE] ONLY ONE DEVICE IS CONNECTED ${NC}"
+./ascent -c "configs_for_tests/valid_config" "dryrun"
+
+echo -e "${B}[INFO] Please connect BOTH devices now...${NC}"
 read
-echo "[TESTCASE] not both devices are connected"
-./ascent -c "test/valid_config" "dryrun"
+echo -e "${G}[TESTCASE] VALID CONFIG ${NC}"
+./ascent -c "configs_for_tests/valid_config" "dryrun"
